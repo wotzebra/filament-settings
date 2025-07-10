cf71189f1327b06ec6f09287a69eb1f2d84addcd
Jyrki De Neve <jyrki.deneve@gmail.com>
2025-07-10 10:25:13 +0300

    Use filament class names to have some spacing between the tabs and submit button

--- resources/views/pages/settings.blade.php
+++ resources/views/pages/settings.blade.php
@@ -1,11 +1,17 @@
 <x-filament::page :header-widgets-columns="4">
 
-    <form wire:submit.prevent="submit" class="space-y-6">
+    <form wire:submit.prevent="submit" class="fi-sc-form">
         {{ $this->form }}
 
-        <x-filament::button type="submit">
-            {{ __('filament-settings::widget.save') }}
-        </x-filament::button>
+        <div class="fi-sc  fi-sc-has-gap fi-grid">
+            <div class="fi-sc-actions">
+                <div class="fi-ac fi-align-start">
+                    <x-filament::button type="submit">
+                        {{ __('filament-settings::widget.save') }}
+                    </x-filament::button>
+                </div>
+            </div>
+        </div>
     </form>
 
 </x-filament::page>
